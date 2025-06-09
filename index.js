d0cc0bdf475de1d2f320d5eeda4f5acc983e4faf
Borys1no <boryscereceda@hotmail.com>
2025-06-09 16:22:34 -0500

    Actuaizacion

--- index.js
+++ index.js
@@ -146,25 +146,104 @@ app.post("/create-appointment", verificarToken, async (req, res) => {
 
     if (!zoomLink) {
       console.error("Error: No se recibió un enlace de Zoom en la respuesta");
-      return res
-        .status(500)
-        .send({
-          error: "Error al crear la reunión de Zoom. No se recibió el enlace",
-        });
+      return res.status(500).send({
+        error: "Error al crear la reunión de Zoom. No se recibió el enlace",
+      });
     }
+
+    const htmlPersonalizadoEnvioEnlace = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          color: #333;
+          background-color: #f9f9f9;
+          padding: 20px;
+        }
+        .container {
+          max-width: 600px;
+          margin: auto;
+          background: white;
+          padding: 30px;
+          border-radius: 10px;
+          box-shadow: 0 2px 8px rgba(0,0,0,0.05);
+        }
+        .header {
+          font-size: 24px;
+          font-weight: bold;
+          margin-bottom: 20px;
+        }
+        .reuma {
+          color: #aece57;
+        }
+        .sur {
+          color: #2a43d2;
+        }
+        .signature {
+          margin-top: 30px;
+          font-size: 14px;
+          color: #555;
+        }
+        .footer {
+          font-size: 12px;
+          color: #777;
+          border-top: 1px solid #eee;
+          margin-top: 30px;
+          padding-top: 10px;
+        }
+        a {
+          color: #2a43d2;
+          text-decoration: none;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <div class="header">
+          <span class="reuma">Reuma</span><span class="sur">sur</span>
+        </div>
+
+        <p>Estimado/a Usuario,</p>
+        <h3>Tu cita ha sido agendada correctamente</h3>
+
+        <p>Adjunto a este correo encontrará su Link de la reunión: <a href="${zoomLink}">Unirse a la reunión de Zoom</a>.</p>
+        <p>Fecha y Hora: ${fechaLocal} (${timeZoneOffset})</p>
+        <p><strong>Convertidor horario:</strong> <a href="https://www.timeanddate.com/worldclock/converter.html?iso=${
+          fechaUTC.toISOString().replace(/[:-]/g, "").split(".")[0]
+        }&p1=1440" target="_blank">Ver en mi zona horaria</a></p>
+
+        <p>Si tiene alguna pregunta o necesita mayor información, no dude en comunicarse con nosotros.</p>
+
+        <br>
+
+        <p>Saludos cordiales,</p>
+
+        <div class="signature">
+          <strong>Reumasur</strong><br>
+          Centro Reumatológico<br>
+          Dir: Bocayá el Colón y Tarqui (Centro de Diagnóstico CEDIAG)<br>
+          Machala - El Oro, Ecuador<br>
+          Tel: 0980304357<br>
+          Email: <a href="mailto:emilio_aroca@yahoo.com">emilio_aroca@yahoo.com</a>
+        </div>
+
+        <div class="footer">
+          Este mensaje es confidencial y está dirigido únicamente al destinatario. Si ha recibido este mensaje por error, por favor elimínelo de inmediato y notifique al remitente.
+        </div>
+      </div>
+    </body>
+    </html>
+    
+    `;
 
     const mailOptions = {
       from: `"Cita Médica" <${process.env.EMAIL_SERVICE_USER}>`,
       to: userEmail,
       subject: "Detalles de tu cita médica",
-      html: `
-        <h3>Tu cita ha sido agendada</h3>
-        <p>Fecha y Hora: ${fechaLocal} (${timeZoneOffset})</p>
-        <p>Link de la reunión: <a href="${zoomLink}">Unirse a la reunión de Zoom</a></p>
-        <p><strong>Convertidor horario:</strong> <a href="https://www.timeanddate.com/worldclock/converter.html?iso=${
-          fechaUTC.toISOString().replace(/[:-]/g, "").split(".")[0]
-        }&p1=1440" target="_blank">Ver en mi zona horaria</a></p>
-      `,
+      html: htmlPersonalizadoEnvioEnlace,
     };
 
     let mailSent = false;
@@ -198,29 +277,46 @@ app.post("/create-appointment", verificarToken, async (req, res) => {
 });
 
 //Endpoint para reagendar una cita
-app.post('/reschedule', verificarToken,  async (req, res) =>{
-  
-    const {appointmentId, originalZoomLink, newStartTime, userEmail, userTimeZone} = req.body;
-    if(!appointmentId || !originalZoomLink || !newStartTime || !userEmail || isNaN(new Date(newStartTime).getTime())) {
+app.post("/reschedule", verificarToken, async (req, res) => {
+  const {
+    appointmentId,
+    originalZoomLink,
+    newStartTime,
+    userEmail,
+    userTimeZone,
+  } = req.body;
+  if (
+    !appointmentId ||
+    !originalZoomLink ||
+    !newStartTime ||
+    !userEmail ||
+    isNaN(new Date(newStartTime).getTime())
+  ) {
+    return res.status(400).send({
+      error:
+        "Faltan campos obligatorios o el formato de fecha es incorrecto. Se requieren: appointmentId, originalZoomLink, newStartTime (ISO 8601), userEmail",
+    });
+  }
+  try {
+    //Extraer el ID de la reunión de Zoom del enlace original
+    const meetingId = extractMeetingIdFromZoomLink(originalZoomLink);
+    if (!meetingId) {
       return res.status(400).send({
-        error: 'Faltan campos obligatorios o el formato de fecha es incorrecto. Se requieren: appointmentId, originalZoomLink, newStartTime (ISO 8601), userEmail'
+        error:
+          "El enlace de Zoom proporcionado no es válido o no contiene un ID de reunión.",
       });
     }
-    try {
-      //Extraer el ID de la reunión de Zoom del enlace original
-      const meetingId = extractMeetingIdFromZoomLink(originalZoomLink);
-      if (!meetingId) {
-        return res.status(400).send({
-          error: 'El enlace de Zoom proporcionado no es válido o no contiene un ID de reunión.'
-        });
-      }
-      //Actualizar la reunión de Zoom con la nueva fecha y hora
-      const updatedMeeting = await updateZoomMeeting( meetingId, newStartTime, zoomToken);
+    //Actualizar la reunión de Zoom con la nueva fecha y hora
+    const updatedMeeting = await updateZoomMeeting(
+      meetingId,
+      newStartTime,
+      zoomToken
+    );
 
-      //Preparar y enviar el correo de confirmación
-      const timeZone = userTimeZone || "America/Guayaquil";
-      const fechaUTC = new Date(newStartTime);
-      const fechaLocal = fechaUTC.toLocaleString("es-EC", {
+    //Preparar y enviar el correo de confirmación
+    const timeZone = userTimeZone || "America/Guayaquil";
+    const fechaUTC = new Date(newStartTime);
+    const fechaLocal = fechaUTC.toLocaleString("es-EC", {
       timeZone: timeZone,
       weekday: "long",
       year: "numeric",
@@ -238,19 +334,99 @@ app.post('/reschedule', verificarToken,  async (req, res) =>{
       })
       .split(" ")[2];
 
-      const mailOptions ={
-        from: `"Cita Médica" <${process.env.EMAIL_SERVICE_USER}>`,
-        to: userEmail,
-        subject: "Reprogramación de tu cita médica",
-        html: `
-        <h3>Tu cita médica ha sido re-agendada</h3>
+    const htmlPersonalizadoReagendar = `
+      <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          color: #333;
+          background-color: #f9f9f9;
+          padding: 20px;
+        }
+        .container {
+          max-width: 600px;
+          margin: auto;
+          background: white;
+          padding: 30px;
+          border-radius: 10px;
+          box-shadow: 0 2px 8px rgba(0,0,0,0.05);
+        }
+        .header {
+          font-size: 24px;
+          font-weight: bold;
+          margin-bottom: 20px;
+        }
+        .reuma {
+          color: #aece57;
+        }
+        .sur {
+          color: #2a43d2;
+        }
+        .signature {
+          margin-top: 30px;
+          font-size: 14px;
+          color: #555;
+        }
+        .footer {
+          font-size: 12px;
+          color: #777;
+          border-top: 1px solid #eee;
+          margin-top: 30px;
+          padding-top: 10px;
+        }
+        a {
+          color: #2a43d2;
+          text-decoration: none;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <div class="header">
+          <span class="reuma">Reuma</span><span class="sur">sur</span>
+        </div>
+
+        <p>Estimado/a su cita medica ha sido reagendado correctamente,</p>
         <p><strong>Nueva Fecha y Hora:</strong> ${fechaLocal} (${timeZoneOffset})</p>
-        <p>Link de la reunión: <a href="${updatedMeeting.join_url || originalZoomLink}">Unirse a la reunión de Zoom</a></p>
+        
+        <p>Adjunto a este correo encontrará su <p>Link de la reunión: <a href="${
+          updatedMeeting.join_url || originalZoomLink
+        }">Unirse a la reunión de Zoom</a></p> </p>
         <p><strong>Convertidor horario:</strong> <a href="https://www.timeanddate.com/worldclock/converter.html?iso=${
           fechaUTC.toISOString().replace(/[:-]/g, "").split(".")[0]
         }&p1=1440" target="_blank">Ver en mi zona horaria</a></p>
-        <p>Si tienes alguna pregunta o necesitas asistencia, por favor contáctanos.</p>
-      `,
+
+        <p>Si tiene alguna pregunta o necesita mayor información, no dude en comunicarse con nosotros.</p>
+
+        <br>
+
+        <p>Saludos cordiales,</p>
+
+        <div class="signature">
+          <strong>Reumasur</strong><br>
+          Centro Reumatológico<br>
+          Dir: Bocayá el Colón y Tarqui (Centro de Diagnóstico CEDIAG)<br>
+          Machala - El Oro, Ecuador<br>
+          Tel: 0980304357<br>
+          Email: <a href="mailto:emilio_aroca@yahoo.com">emilio_aroca@yahoo.com</a>
+        </div>
+
+        <div class="footer">
+          Este mensaje es confidencial y está dirigido únicamente al destinatario. Si ha recibido este mensaje por error, por favor elimínelo de inmediato y notifique al remitente.
+        </div>
+      </div>
+    </body>
+    </html>
+      `;
+
+    const mailOptions = {
+      from: `"Cita Médica" <${process.env.EMAIL_SERVICE_USER}>`,
+      to: userEmail,
+      subject: "Reprogramación de tu cita médica",
+      html: htmlPersonalizadoReagendar,
     };
     let mailSent = false;
     try {
@@ -258,7 +434,10 @@ app.post('/reschedule', verificarToken,  async (req, res) =>{
       console.log(`Correo de re-agendamiento enviado a ${userEmail}`);
       mailSent = true;
     } catch (mailError) {
-      console.error(`Error al enviar el correo a ${userEmail}: `, mailError.message);
+      console.error(
+        `Error al enviar el correo a ${userEmail}: `,
+        mailError.message
+      );
     }
     res.status(200).send({
       message: mailSent
@@ -267,18 +446,17 @@ app.post('/reschedule', verificarToken,  async (req, res) =>{
       zoomLink: updatedMeeting.join_url || originalZoomLink,
       correoEnviado: mailSent,
     });
-
   } catch (error) {
-    console.error(" Error al reagendar la cita:",
+    console.error(
+      " Error al reagendar la cita:",
       error.response
         ? `${error.response.status} - ${JSON.stringify(error.response.data)}`
         : error.message
     );
     res.status(500).send({
       error: "Error al reagendar la cita.",
-      detalle: error.message
+      detalle: error.message,
     });
-    
   }
 });
 
@@ -286,7 +464,7 @@ app.post('/reschedule', verificarToken,  async (req, res) =>{
 function extractMeetingIdFromZoomLink(zoomLink) {
   try {
     const url = new URL(zoomLink);
-    const pathParts = url.pathname.split('/');
+    const pathParts = url.pathname.split("/");
     return pathParts[pathParts.length - 1];
   } catch (e) {
     console.error("Error al extraer meeting ID del enlace:", e);
@@ -305,36 +483,34 @@ async function updateZoomMeeting(meetingId, newStartTime, token) {
       {
         headers: {
           Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       }
     );
     return response.data;
   } catch (error) {
-    console.error("Error al actualizar reunión de Zoom:", error.response?.data || error.message);
+    console.error(
+      "Error al actualizar reunión de Zoom:",
+      error.response?.data || error.message
+    );
     throw error;
   }
 }
 
-
 app.post("/enviar-receta", async (req, res) => {
-
   const { email, nombrePaciente, recetaPDFBase64 } = req.body;
-  
+
   if (!email || !nombrePaciente || !recetaPDFBase64) {
     return res.status(400).json({
-      error: "Faltan campos obligatorios: email, nombrePaciente o receta"
+      error: "Faltan campos obligatorios: email, nombrePaciente o receta",
     });
   }
 
   try {
-    
-
-    const pdfBuffer = Buffer.from(recetaPDFBase64, 'base64');
+    const pdfBuffer = Buffer.from(recetaPDFBase64, "base64");
 
     //Plantilla HTML personalizada
-    const htmlPersonalizado = 
-    `
+    const htmlPersonalizado = `
     <!DOCTYPE html>
     <html>
     <head>
@@ -425,20 +601,19 @@ app.post("/enviar-receta", async (req, res) => {
         {
           filename: `Receta-${nombrePaciente}.pdf`,
           content: pdfBuffer,
-          contentType: 'application/pdf',
+          contentType: "application/pdf",
         },
       ],
     };
 
     await transporter.sendMail(mailOptions);
     console.log(`Receta enviada a ${email}`);
-    res.status(200).send({ message: 'Receta enviada exitosamente.' });
-
+    res.status(200).send({ message: "Receta enviada exitosamente." });
   } catch (error) {
-    console.error('Error al enviar la receta:', error);
-    res.status(500).send({ 
-      error: 'No se pudo enviar la receta.',
-      detalle: error.message 
+    console.error("Error al enviar la receta:", error);
+    res.status(500).send({
+      error: "No se pudo enviar la receta.",
+      detalle: error.message,
     });
   }
 });
